3219a7edbfc305b9c48f41f6361fb85b03e6dfba
SineshX <Sineshkumarr@gmail.com>
2023-08-13 22:05:52 +0530

    test

--- main.js
+++ main.js
@@ -1,132 +1,68 @@
 // Get elements from the DOM
-const accessButton = document.getElementById('access-button');
+const accessButton = document.getElementById('access-note');
 const noteEditor = document.querySelector('.note-editor');
 const saveNoteButton = document.getElementById('save-note');
 const existingNote = document.querySelector('.existing-note');
 const noteTextarea = document.getElementById('note-textarea');
+const titleInput = document.getElementById('title');
 
-class NotesApp {
-    notesRef = db.collection("notes");
-
-    async addOrUpdateNote(key, text) {
-        const note = { text };
-
-        try {
-            await this.notesRef.doc(key).set(note, { merge: true });
-            console.log('Note Added/Updated for Key: ', key);
-        } catch (error) {
-            console.error('Error Adding/Updating Note: ', error);
-        }
-    }
-
-    async getNoteByKey(key) {
-        let note;
-
-        try {
-            const doc = await this.notesRef.doc(key).get();
-            
-            if (doc.exists) 
-                note = { key: doc.id, ...doc.data() };
-            else
-                console.log('No note found with key: ', key);
-        } 
-        catch (error) {
-            console.error('Error in getting note: ', error);
-        }
-
-        return note;
-    }
-}
-
-async function main() {
-    const notesApp = new NotesApp();
-
-    // Load and display the example note with the key "exampleKey"
-    const exampleNote = await notesApp.getNoteByKey("exampleKey");
-    if (exampleNote) {
-        noteTextarea.value = exampleNote.text;
-        existingNote.innerHTML = `<p>${exampleNote.text}</p>`;
-        existingNote.style.display = 'block';
-    }
-
-    accessButton.addEventListener('click', async () => {
-        // ... Existing access button code ...
-    });
-
-    saveNoteButton.addEventListener('click', async () => {
-        // ... Existing save note button code ...
-    });
-}
-
-main();
 
 
 class NotesApp {
-    notesRef = db.collection("notes");
-
-    async addOrUpdateNote(key, text) {
-        const note = { text };
-
-        try {
-            await this.notesRef.doc(key).set(note, { merge: true });
-            console.log('Note Added/Updated for Key: ', key);
-        } catch (error) {
-            console.error('Error Adding/Updating Note: ', error);
-        }
+    constructor() {
+        this.notesRef = db.collection("notes");
     }
 
-    async getNoteByKey(key) {
-        let note;
-
+    async getNoteByTitle(title) {
         try {
-            const doc = await this.notesRef.doc(key).get();
-            
-            if (doc.exists) 
-                note = { key: doc.id, ...doc.data() };
-            else
-                console.log('No note found with key: ', key);
-        } 
-        catch (error) {
-            console.error('Error in getting note: ', error);
-        }
-
-        return note;
-    }
-}
-
-async function main() {
-    const notesApp = new NotesApp();
-
-    accessButton.addEventListener('click', async () => {
-        const key = prompt('Enter access key:');
-        if (key) {
-            const note = await notesApp.getNoteByKey(key);
-            if (note) {
-                noteTextarea.value = note.text;
+            const doc = await this.notesRef.doc(title).get();
+            if (doc.exists) {
+                return doc.data().text;
             } else {
-                noteTextarea.value = '';
+                return null;
             }
+        } catch (error) {
+            console.error('Error getting note:', error);
+            return null;
+        }
+    }
+
+    async addOrUpdateNote(title, text) {
+        const note = { text };
+
+        try {
+            await this.notesRef.doc(title).set(note, { merge: true });
+            console.log('Note Added/Updated for title:', title);
+        } catch (error) {
+            console.error('Error Adding/Updating Note:', error);
+        }
+    }
+}
+
+async function main() {
+    const notesApp = new NotesApp();
+
+    accessButton.addEventListener('click', async () => {
+        const title = titleInput.value.trim();
+        if (title) {
+            const noteText = await notesApp.getNoteByTitle(title);
+            noteTextarea.value = noteText || '';
             noteEditor.style.display = 'block';
         }
     });
 
     saveNoteButton.addEventListener('click', async () => {
-        const key = prompt('Enter access key:');
+        const title = titleInput.value.trim();
         const text = noteTextarea.value.trim();
 
-        if (key !== '' && text !== '') {
-            await notesApp.addOrUpdateNote(key, text);
+        if (title && text) {
+            await notesApp.addOrUpdateNote(title, text);
             noteEditor.style.display = 'none';
         }
     });
 
-    // Example: Load and display a note with the key "exampleKey"
-    const exampleNote = await notesApp.getNoteByKey("exampleKey");
-    if (exampleNote) {
-        noteTextarea.value = exampleNote.text;
-        existingNote.innerHTML = `<p>${exampleNote.text}</p>`;
-        existingNote.style.display = 'block';
-    }
+    // Load existing notes
+    // You can implement this based on your needs, fetching all notes and displaying them.
 }
 
 main();
--- readme.md
+++ readme.md
@@ -1,12 +1,7 @@
-npm install -g firebase-tools
-firebase login
 
-firebase init
-
-firebase deploy
-
-```inside body tag in last ```
-
-
+<link rel="stylesheet" href="styles.css">
 <script src="https://www.gstatic.com/firebasejs/8.2.0/firebase-app.js"></script>
 <script src="https://www.gstatic.com/firebasejs/8.1.2/firebase-firestore.js"></script>
+
+<script src="firebase-config.js"></script>
+<script src="main.js"></script>
